12b011378e202fc81f32776fffc48bc9e8fea42e
prakharchaurasiya <71957167+prakharchaurasiya@users.noreply.github.com>
2022-08-17 15:11:01 +0530

    Create probability_distribution.py

---
+++ Probability/probability_distribution.py
@@ -0,0 +1,22 @@
+'''
+Calculating probability of getting 50 heads in a 100 coin toss experiment.
+And plotting the probability distribution curve.
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def toss_100_times():
+    return np.random.choice(['H', 'T'], 100)
+
+n = 1000000
+distribution = np.zeros(n)
+for i in range(n):
+    result = toss_100_times()
+    distribution[i] = np.count_nonzero(result == 'H')
+
+plt.figure(figsize=(10,8))
+plt.hist(distribution, bins = 50)
+plt.show()
+
+print(f'Probability of getting 50 heads in 100 toss: {np.count_nonzero(distribution == 50)/n}')
